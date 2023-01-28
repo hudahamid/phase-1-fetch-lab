83b9990a7ede9573d49b2cffda562801960b9dc3
hudahamid <huda.00091581@gmail.com>
2023-01-28 22:52:11 +0300

    ok

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
+
+
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books")
-    .then((res) => res.json())
-    .then((res) => {
-      renderBooks(res);
-    });
+ return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json))
 }
 
 function renderBooks(books) {
-  const main = document.querySelector("main");
-  books.forEach((book) => {
-    const h2 = document.createElement("h2");
+  const main = document.querySelector('main');
+  books.forEach(book => {
+    const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
